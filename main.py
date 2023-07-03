ff25eeb7e1abe681ae1a4a461cabfe2f7e3fcf67
BlackCat <amey992004@gmail.com>
2023-07-03 18:01:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from os import getenv, system
 GIT = Github(auth=Auth.Token(getenv("TOKEN")))
 REPO_NAME = "LineageOS"
 MY_REPO = "Project-FelineX"
+meowcheck = False
 
 lineage = GIT.get_organization(REPO_NAME)
 org = GIT.get_organization(MY_REPO)
@@ -14,8 +15,12 @@ for repo in lineage.get_repos():
     repomain = repo.full_name.replace(f"{REPO_NAME}/", "")
     if "device" not in repomain and "kernel" not in repomain:
         print(f"{REPO_NAME}/{repomain}")
-        if org.get_repo(repomain):
-            org.get_repo(repomain).delete()
+        try:
+            meowcheck = org.get_repo(repomain)
+        else:
+            meowcheck = False
+        if meowcheck:
+            meowcheck.delete()
             print("Deleted")
         try:
             org.create_fork(repo, default_branch_only=True)
