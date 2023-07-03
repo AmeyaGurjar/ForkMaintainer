768b6e8eb9599866a2f0cb0442d8a613058b22c0
BlackCat <amey992004@gmail.com>
2023-07-03 18:18:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from time import sleep
 from github import Github, Auth
-from os import getenv, system
+from os import getenv, system, remove
 
 GIT = Github(auth=Auth.Token(getenv("TOKEN")))
 REPO_NAME = "LineageOS"
@@ -11,7 +11,11 @@ meowcheck = False
 
 lineage = GIT.get_organization(REPO_NAME)
 org = GIT.get_organization(MY_REPO)
-
+try:
+    remove("logs.txt")
+except:
+    pass
+logFile = open("logs.txt", "a")
 for repo in lineage.get_repos():
     repomain = repo.full_name.replace(f"{REPO_NAME}/", "")
     if "device" not in repomain and "kernel" not in repomain:
@@ -22,13 +26,16 @@ for repo in lineage.get_repos():
             meowcheck = False
         if meowcheck:
             meowcheck.delete()
+            logFile.write(f"[DELETED] - {repomain}")
             print("Deleted")
         sleep(5)
         try:
             org.create_fork(repo, default_branch_only=True)
+            logFile.write(f"[FORKED] - {repomain}")
             print("Forked!")
         except Exception as e:
-            print(e)
+            logFile.write(f"[ERROR] - {repomain}")
             print("skipped")
     else:
         continue
+logFile.close()
