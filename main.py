d52ced68133be8bf83fbfe4c9b06d6eac66f228f
AmeyaGurjar <amey992004@gmail.com>
2023-07-03 16:58:15 +0530

    meoa

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+load_dotenv()
+from github import Github
+from os import getenv, system
+
+GIT = Github(getenv("TOKEN"))
+REPO_NAME = "Lineage"
+MY_REPO = "Project-FelineX"
+
+user = GIT.get_user(REPO_NAME)
+
+for repo in user.get_repos():
+    repo = repo.replace(f"{REPO_NAME}/", "")
+    print(repo)
