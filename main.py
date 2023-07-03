1cfddea99228c6d9207446a233565af49f054507
BlackCat <amey992004@gmail.com>
2023-07-03 17:11:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ user = GIT.get_user(REPO_NAME)
 
 for repo in user.get_repos():
     repo = repo.full_name.replace(f"{REPO_NAME}/", "")
-    if "device" not in repo or "kernel" not in repo:
+    if "device" not in repo and "kernel" not in repo:
         print(repo)
     else:
         continue
